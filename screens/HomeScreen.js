b9a08c963182ad133a82e5bd4ac3fab46f4d9ed8
Kevin Chao <47195482+chaokevin1@users.noreply.github.com>
2025-05-21 10:46:53 -0700

    Update HomeScreen.js
    fix logout button redirect to root sing in page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import {  CommonActions } from '@react-navigation/native';
 
 export default function HomeScreen() {
 
-const navigation = useNavigation();
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    // Perform logout logic, e.g., clear tokens, etc.
 
-const handleSignOut = () =>{
-    navigation.navigate("Login")
+    navigation.dispatch(
+      CommonActions.reset({
+           index: 0,
+           routes: [{ name: 'Login' }],
+         })
+    );
+  };
 
-}
 
 
   return (
@@ -19,6 +26,7 @@ const handleSignOut = () =>{
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
